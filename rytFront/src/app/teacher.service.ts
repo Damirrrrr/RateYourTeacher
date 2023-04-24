4a729804e9c0eaef6ccf60cfd5ef5c481ac828e3
Amina Niyazbek <aminokn@MacBook-Air-Amina.local>
2023-04-24 20:03:37 +0600

    updating

--- rytFront/src/app/teacher.service.ts
+++ rytFront/src/app/teacher.service.ts
@@ -15,7 +15,5 @@ export class TeacherService {
   getTeacher(id: number): Observable<Teacher>{
     return this.client.get<Teacher>(`${this.BASE_URL}/api/teachers/${id}/`)
   }
-  getTeachersUniversities(id: number): Observable<Teacher[]> {
-    return this.client.get<Teacher[]>(`${this.BASE_URL}/api/teachers/${id}/universities/`)
-  }
+
 }
--- rytFront/src/app/teacher.ts
+++ rytFront/src/app/teacher.ts
@@ -2,9 +2,28 @@ export interface Teacher{
     id: number;
     fullname: string;
     university: string;
+    img: string;
     rating: number;
-    discipline: string;
-    degree: string;
     description: string;
     url: string;
 }
+export const teachers = [
+  {
+    id: 1,
+    fullname: 'Bobur Mukhsimbayev',
+    university : 'KBTU',
+    img : "",
+    rating: 5,
+    description: 'asdfgh',
+    url: 'https://github.com/bobur554396'
+},
+{
+    id: 2,
+    fullname: 'Beisenbek Baisakov',
+    university : 'KBTU',
+    img : "",
+    rating: 5,
+    description: 'asdfgh',
+    url: 'https://github.com/Beisenbek'
+}
+]
