1db699d4031cc72da819f520b567dae47c23e5b7
Damirrrrr <amanzholovdamir@inbox.ru>
2023-04-28 19:30:22 +0600

    fixed

--- rytFront/src/app/teacher.ts
+++ rytFront/src/app/teacher.ts
@@ -7,8 +7,6 @@ export interface Teacher{
     description: string;
     url: string;
 }
-<<<<<<< HEAD
-
 export const teachers = [
     {
       id: 1,
@@ -29,25 +27,3 @@ export const teachers = [
       url: 'https://github.com/Beisenbek'
   }
   ]
-=======
-export const teachers = [
-  {
-    id: 1,
-    fullname: 'Bobur Mukhsimbayev',
-    university : 'KBTU',
-    img : "",
-    rating: 5,
-    description: 'asdfgh',
-    url: 'https://github.com/bobur554396'
-},
-{
-    id: 2,
-    fullname: 'Beisenbek Baisakov',
-    university : 'KBTU',
-    img : "",
-    rating: 5,
-    description: 'asdfgh',
-    url: 'https://github.com/Beisenbek'
-}
-]
->>>>>>> 8cb268aba4488bf10bb9368a21fa4a497f70fbf1
